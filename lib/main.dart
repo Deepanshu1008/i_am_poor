81abc85a44a9361db02496d86fd823a47bfaea0f
Deepanshu <deepanshudp10@gmail.com>
2022-04-18 01:22:53 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+// the main function is the starting for all our Flutter apps
+void main() {
+  //looks inside main
+  runApp(
+    // it should run an app
+    MaterialApp(
+      //the app it should run is a material app
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('                             I Am Poor'),
+          backgroundColor: Colors.black87,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/coal.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
